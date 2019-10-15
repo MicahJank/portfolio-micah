26066733f617f5aa5315b9330e76a21b3397c61a
MicahJank <MicahJank@users.noreply.github.com>
2019-10-14 19:51:40 -0500

    fixed minor issue with the cursor showing as pointer on divs it wasnt supposed to

--- assets/js/projectcards.js
+++ assets/js/projectcards.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('.box').click(function() {
+    $('.project').click(function() {
         console.log(this);
         window.open($(this).find('a.project-card').attr('href'));
         return false;
